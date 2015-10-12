de5d7b12a6aedfa6830df8457653ee5b004c8a95
InusualZ <wesmoret98@gmail.com>
2015-10-12 19:51:10 -0400

    Added License

--- include/Network/Minecraft/StartGame.h
+++ include/Network/Minecraft/StartGame.h
@@ -1,3 +1,14 @@
+/**
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* @author InusualZ
+*
+*
+*/
+
 #ifndef STARTGAME_H_
 #define STARTGAME_H_
 
--- src/Network/Minecraft/LoginStatus.cpp
+++ src/Network/Minecraft/LoginStatus.cpp
@@ -1,3 +1,14 @@
+/**
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* @author InusualZ
+*
+*
+*/
+
 #include "Network\Minecraft\LoginStatus.h"
 #include "Network\Minecraft\MinecraftPackets.h"
 
