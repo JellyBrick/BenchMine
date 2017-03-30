641753425423a985a839d4d89d4e08c31e23328f
InusualZ <wesmoret98@gmail.com>
2017-03-29 22:19:54 -0400

    Implemented base64 encoding/decoding

--- source/common/Utils.h
+++ source/common/Utils.h
@@ -9,6 +9,18 @@ public:
 	// String Utils
 	static std::vector<std::string> explode(const std::string& string, const char delimiter);
 
+	/*
+	 * SOURCE: http://renenyffenegger.ch/notes/development/Base64/Encoding-and-decoding-base-64-with-cpp
+	 * AUTHOR: René Nyffenegger
+	 */
+	static std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len);
+
+	/*
+	 * SOURCE: http://renenyffenegger.ch/notes/development/Base64/Encoding-and-decoding-base-64-with-cpp
+	 * AUTHOR: René Nyffenegger
+	 */
+	static std::string base64_decode(std::string const& encoded_string);
+
 	// Console Utils
 	static void setConsoleTitle(const std::string& title);
 };
